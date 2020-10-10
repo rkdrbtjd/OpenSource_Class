ffc9807e5e0c623548ac048bfd0eaf5642c251cb
rkdrbtjd <72647202+rkdrbtjd@users.noreply.github.com>
2020-10-10 18:19:12 +0900

    Update test.py
    findMax 함수의 변수명 변경(bigget>big)

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 def findMax(a,b,c):
     if a>b:
-        biggest=a
+        big=a
     else:
-        biggest=b
-    if c>biggest:
-        biggest=c
+        big=b
+    if c>big:
+        big=c
 
-    return biggest
+    return big
 
 a=int(input("첫번째 숫자 입력:"))
 b=int(input("두번째 숫자 입력:"))
